a249daba3189aae3321e9cbd4771780773bb61ff
Christian Fei <crifei93@gmail.com>
2016-10-23 20:35:39 +0200

    add utils test

---
+++ test/utils.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import test from 'ava';
+import {isExternalLink} from '../utils';
+
+test('utils.isExternalLink returns false for internal urls', t => {
+	t.false(isExternalLink('https://pomodoro.cc'));
+	t.false(isExternalLink('https://pomodoro.cc/about'));
+});
+
+test('utils.isExternalLink returns false for internal urls with http...', t => {
+	t.false(isExternalLink('http://pomodoro.cc'));
+	t.false(isExternalLink('http://pomodoro.cc/about'));
+});
+
+test('utils.isExternalLink returns false for internal urls except /docs', t => {
+	t.true(isExternalLink('https://pomodoro.cc/docs'));
+	t.true(isExternalLink('https://medium.com/@pomodoro_cc'));
+});
---
+++ utils.js
@@ -0,0 +1,20 @@
+'use strict';
+const shell = require('electron').shell;
+
+const isExternalLink = url => {
+	const isInternalLink = /^https?:\/\/pomodoro\.cc/.test(url) && url !== 'https://pomodoro.cc/docs';
+	return !isInternalLink;
+};
+const openUrlInExternalWindow = (event, url) => {
+	if (isExternalLink(url)) {
+		return false;
+	}
+
+	event.preventDefault();
+	shell.openExternal(url);
+};
+
+module.exports = {
+	isExternalLink: isExternalLink,
+	openUrlInExternalWindow: openUrlInExternalWindow
+};
